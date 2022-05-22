ce6d4206a0f483c5f09a1640f69a40389e15548e
ArtofHarry7 <harshit.singh.cse20@itbhu.ac.in>
2022-05-22 20:11:53 +0530

    ReadMe

--- options.py
+++ options.py
@@ -3,28 +3,13 @@ import template
 
 parser = argparse.ArgumentParser(description ='Efficient Residual Dense Block Search for Image Super-Resolution')
 
-# parser.add_argument('--debug',
-#                     action='store_true',
-#                     help='Enables debug mode')
-# parser.add_argument('--template',
-#                     default='.',
-#                     help='You can set various templates in option.py')
-
 parser.add_argument("-t", "--task",
                     required=True,
                     choices=['test', 'train', 'evolve'],
                     metavar="Task",
-                    help="Task to perform"
-                    )
-# parser.add_argument("-e", "--evolve",
-#                     metavar="Evolve",
-#                     help = "Get a model using guided evolutionory method")
-# parser.add_argument("-r", "--retrain",
-#                     metavar="Retrain",
-#                     help = "Get a model using guided evolutionory method")
-# parser.add_argument("-t", "--test",
-#                     metavar="Test",
-#                     help = "Test a model")
+                    help="Task to perform")
+
+
 parser.add_argument("-s", "--sequence",
                     metavar='Sequence',
                     help="sequence you want to train or test")
